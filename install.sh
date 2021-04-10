bc86434c8d1db4f50ad1efa21f429742bdfc941e
Phat Vo <votanphatb2@gmail.com>
2021-04-10 14:57:25 +0700

    feat: install script

--- install.sh
+++ install.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
-#Homebrew
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-#Oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#ZSH Autosuggestion
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-#Brew formulae, casks
-brew install $(cat brew.txt)
-brew install --cask $(cat brew.cask.txt)
 #Clone config to home directory
+rm -rf ~/.config
 cp -R .config ~
+rm ~/.tmux.conf ~/.zshenv
 cp {.tmux.conf,.zshenv} ~
