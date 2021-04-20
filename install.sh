bd88c82bd9538fe2eacdce3003ab7a47d5786e01
Phat Vo <votanphatb2@gmail.com>
2021-04-20 11:46:29 +0700

    Update .tmux.conf

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-#Clone config to home directory
-rm -rf ~/.config
-cp -R .config ~
-rm ~/.tmux.conf ~/.zshenv
-cp {.tmux.conf,.zshenv} ~
+#Clone neovim config
+rm -rf ~/.config/nvim
+cp -R .config/nvim ~/.config
