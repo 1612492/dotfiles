b731e14206cf595acc97ac82e1e503537c877a87
Phat Vo <votanphatb2@gmail.com>
2021-04-21 01:59:35 +0700

    New colorcheme

--- .config/nvim/lua/plugin/airline.lua
+++ .config/nvim/lua/plugin/airline.lua
@@ -1,4 +1,5 @@
-vim.g.airline_theme = 'material'
+vim.g.airline_theme = 'github'
+-- vim.g.airline_theme = 'material'
 vim.g.airline_filetype_overrides = { ['coc-explorer'] = {'explorer', ''}}
 vim.g['airline#extensions#ale#enabled'] = 1
 vim.g['airline#extensions#hunks#enabled'] = 0
---
+++ .config/nvim/lua/plugin/closetag.lua
@@ -0,0 +1,2 @@
+vim.g.closetag_filenames = '*.html,*.js,*.ts,*.jsx,*.tsx'
+vim.g.closetag_filetypes = 'html,javascript,typescript,javascriptreact,typescriptreact'
