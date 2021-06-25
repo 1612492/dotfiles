6586a1f301a110c5637ce07e9453798347e200d4
Phat Vo <votanphatb2@gmail.com>
2021-06-25 10:21:45 +0700

    Migrate to lua

--- .config/nvim/lua/mapping.lua
+++ .config/nvim/lua/mapping.lua
@@ -1,44 +1,22 @@
 vim.g.mapleader = ' '
 
-vim.api.nvim_set_keymap('n','<leader>e',':CocCommand explorer<cr>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n','<leader>g',':CocList grep<cr>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n','<leader>f',':CocList files<cr>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n','<leader>c',':CocList commands<cr>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n','<leader>r',':CocListResume<cr>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n','<leader>bl',':CocList buffers<cr>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n','<leader>ba',':%bd|e#|bd#<cr>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n','<leader>bd',':bp|bd#<cr>', {noremap = true, silent = true})
+local opts = { noremap=true, silent=true }
+local map = vim.api.nvim_set_keymap
 
-vim.api.nvim_set_keymap('n', 'gd', '<Plug>(coc-definition)', {})
-vim.api.nvim_set_keymap('n', 'gy', '<Plug>(coc-type-definition)', {})
-vim.api.nvim_set_keymap('n', 'gi', '<Plug>(coc-implementation)', {})
-vim.api.nvim_set_keymap('n', 'gr', '<Plug>(coc-references)', {})
+-- nvim-tree.lua
+map('n','<leader>e',':NvimTreeToggle<CR>', opts)
 
-vim.api.nvim_set_keymap('n', '[g', '<Plug>(coc-git-prevchunk)', {})
-vim.api.nvim_set_keymap('n', ']g', '<Plug>(coc-git-nextchunk)', {})
-vim.api.nvim_set_keymap('n', '[c', '<Plug>(coc-git-prevconflict)', {})
-vim.api.nvim_set_keymap('n', ']c', '<Plug>(coc-git-nextconflict)', {})
+-- nvim-lspconfig
+map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+map('n', 'gy', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+map('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+map('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
 
-vim.api.nvim_set_keymap('x', '<leader>a', '<Plug>(coc-codeaction-selected)',{})
-vim.api.nvim_set_keymap('n', '<leader>a', '<Plug>(coc-codeaction-selected)',{})
-
-vim.api.nvim_set_keymap('x', 'if', '<Plug>(coc-funcobj-i)',{})
-vim.api.nvim_set_keymap('o', 'if', '<Plug>(coc-funcobj-i)',{})
-vim.api.nvim_set_keymap('x', 'af', '<Plug>(coc-funcobj-a)',{})
-vim.api.nvim_set_keymap('o', 'af', '<Plug>(coc-funcobj-a)',{})
-
-vim.api.nvim_set_keymap('i', '<cr>', "pumvisible() ? '<c-y>' : '<cr>'", {noremap = true, expr = true})
-
--- Buffers
-vim.api.nvim_set_keymap('n', '<leader>1', '<Plug>AirlineSelectTab1', {})
-vim.api.nvim_set_keymap('n', '<leader>2', '<Plug>AirlineSelectTab2', {})
-vim.api.nvim_set_keymap('n', '<leader>3', '<Plug>AirlineSelectTab3', {})
-vim.api.nvim_set_keymap('n', '<leader>4', '<Plug>AirlineSelectTab4', {})
-vim.api.nvim_set_keymap('n', '<leader>5', '<Plug>AirlineSelectTab5', {})
-vim.api.nvim_set_keymap('n', '<leader>6', '<Plug>AirlineSelectTab6', {})
-vim.api.nvim_set_keymap('n', '<leader>7', '<Plug>AirlineSelectTab7', {})
-vim.api.nvim_set_keymap('n', '<leader>8', '<Plug>AirlineSelectTab8', {})
-vim.api.nvim_set_keymap('n', '<leader>9', '<Plug>AirlineSelectTab9', {})
-vim.api.nvim_set_keymap('n', '<leader>0', '<Plug>AirlineSelectTab0', {})
-vim.api.nvim_set_keymap('n', '<leader>-', '<Plug>AirlineSelectPrevTab', {})
-vim.api.nvim_set_keymap('n', '<leader>+', '<Plug>AirlineSelectNextTab', {})
+-- telescope.nvim
+map('n', '<leader>f', '<cmd>Telescope find_files<cr>', opts)
+map('n', '<leader>g', '<cmd>Telescope live_grep<cr>', opts)
+map('n', '<leader>b', '<cmd>Telescope buffers<cr>', opts)
---
+++ .config/nvim/lua/plugin/tree.lua
@@ -0,0 +1,12 @@
+vim.g.nvim_tree_width = 40
+vim.g.nvim_tree_auto_close = 1
+vim.g.nvim_tree_auto_ignore_ft = { 'startify', 'dashboard' }
+vim.g.nvim_tree_follow = 1
+vim.g.nvim_tree_indent_markers = 1
+vim.g.nvim_tree_git_hl = 1
+vim.g.nvim_tree_highlight_opened_files = 1
+vim.g.nvim_tree_width_allow_resize  = 1
+vim.g.nvim_tree_lsp_diagnostics = 1
+vim.g.nvim_tree_icon_padding = ' '
+vim.g.nvim_tree_update_cwd = 1
+vim.g.nvim_tree_special_files = {}
