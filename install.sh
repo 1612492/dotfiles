1030c51fe1d316c45bbe180f10652a6b613f13de
Phat Vo <39008689+1612492@users.noreply.github.com>
2023-09-28 11:50:36 +0700

    feat: ensure install tmux plugins

--- install.sh
+++ install.sh
@@ -4,16 +4,19 @@ DOTFILES_REPO_URL="https://github.com/1612492/dotfiles.git"
 DOTFILES_DIR=~/dotfiles
 
 if [ ! -d "$DOTFILES_DIR" ]; then
-  echo "Dotfiles repository not found. Cloning it from $DOTFILES_REPO_URL..."
   git clone "$DOTFILES_REPO_URL" "$DOTFILES_DIR"
-else
-  echo "Dotfiles repository already exists."
 fi
 
-echo "Creating symbolic links..."
 ln -s "$DOTFILES_DIR/kitty" ~/.config/kitty
 ln -s "$DOTFILES_DIR/nvim" ~/.config/nvim
 ln -s "$DOTFILES_DIR/tmux/tmux.conf" ~/.tmux.conf
 ln -s "$DOTFILES_DIR/zsh/.zshrc" ~/.zshrc
+
+if [ ! -d ~/.tmux/plugins ]; then
+  mkdir -p ~/.tmux/plugins
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  tmux source-file ~/.tmux.conf
+  ~/.tmux/plugins/tpm/bin/install_plugins
+fi
 
 echo "Setup successfully."
