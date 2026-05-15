1dee10265efae116bc15402df381ce5210c8dc13
1612492 <39008689+1612492@users.noreply.github.com>
2026-05-16 03:11:50 +0700

    chore: hyper key

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,24 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   lazy = false,
   build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter").install({
-      "bash",
-      "css",
-      "go",
-      "html",
-      "javascript",
-      "json",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "rust",
-      "svelte",
-      "tsx",
-      "typescript",
-      "vim",
-      "vimdoc",
-      "yaml",
-    })
-  end,
+  opts = {
+    install_dir = vim.fn.stdpath("data") .. "/treesitter",
+  },
 }
